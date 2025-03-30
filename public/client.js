3188ea6e663f7207796f8b07a03358de907324e4
DEVRAJ HONMANE <devrajhonmane@gmail.com>
2025-03-31 03:14:49 +0530

    Update client.js

--- public/client.js
+++ public/client.js
@@ -1,46 +1,46 @@
+const socket = io(window.location.origin); // Automatically uses the correct backend URL
 
-const socket = io('http://localhost:3003');
 const form = document.getElementById('send-container');
 const messageInput = document.getElementById('messageInp');
 const messageContainer = document.querySelector('.container'); 
 
-form.addEventListener('submit' , (e)=>{
+form.addEventListener('submit', (e) => {
    e.preventDefault();
    const message = messageInput.value;
-   append(`You : ${message} ` , 'right');
-   socket.emit('send' , message);
-   messageInput.value = ''
+   append(`You: ${message}`, 'right');
+   socket.emit('send', message);
+   messageInput.value = '';
 });
+
 var audio = new Audio('/tone.mp3');
+
 const append = (message, position) => {
    const messageElement = document.createElement('div');
    messageElement.innerText = message;
-   messageElement.classList.add('message');
-   messageElement.classList.add(position);
+   messageElement.classList.add('message', position);
    messageContainer.append(messageElement);
-   if (position == 'left'){
-      audio.play()
-   }
-}
 
-const Name = prompt("Enter your name to join"); 
-if (Name){
-socket.emit('new-user-joined', Name);
-}
-else {
-   alert("you must enter the name to join the chat !");
+   if (position === 'left') {
+      audio.play();
+   }
+};
+
+const Name = prompt("Enter your name to join");
+if (Name) {
+   socket.emit('new-user-joined', Name);
+} else {
+   alert("You must enter your name to join the chat!");
    location.reload();
 }
-
 
 socket.on('user-joined', data => {
    append(`${data} joined the chat`, 'right'); 
 });
 
-socket.on('recieve' , data=>{
+socket.on('receive', data => {  // Fixed spelling mistake from 'recieve' -> 'receive'
    append(`${data.name}: ${data.message}`, 'left');
 });
-socket.on('left' , data=>{
-   append(`${data} has left the chat ` , 'left');
-});
 
+socket.on('left', data => {
+   append(`${data} has left the chat`, 'left');
+});
